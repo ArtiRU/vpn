a7e463586146118dc7e3ae1669631ef3ef882c58
artiqt <gignt0646@mail.ru>
2026-02-18 19:41:31 +0300

    fix swagger auth

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -3,7 +3,7 @@ import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
   @ApiProperty({
-    example: 'user@example.com',
+    example: 'gignt0646@mail.ru',
     description: 'User email address',
   })
   @IsEmail()
@@ -11,7 +11,7 @@ export class LoginDto {
   email: string;
 
   @ApiProperty({
-    example: 'password123',
+    example: 'Ice8666099!',
     description: 'User password',
     minLength: 6,
   })
